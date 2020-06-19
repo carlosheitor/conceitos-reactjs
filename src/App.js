c77aa068318a2044394d8b22930f4187016f35e0
Carlos Heitor <c.heitordiniz@gmail.com>
2020-06-19 02:23:12 -0300

    Implement list, create and remove integrations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, [handleRemoveRepository]);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = {
+      url: "https://github.com/Rocketseat/umbriel",
+      title: `Umbriel ${Date.now()}`,
+      techs: ["Node", "Express", "TypeScript"],
+      likes: 0,
+    };
+    api.post("repositories", repository).then((response) => {
+      console.log(response);
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`).then((response) => {
+      setRepositories(response.data);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories &&
+          repositories.map((repo) => {
+            return (
+              <li key={repo.id}>
+                {repo.title}
+                <button onClick={() => handleRemoveRepository(repo.id)}>
+                  Remover
+                </button>
+              </li>
+            );
+          })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
