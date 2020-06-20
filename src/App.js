21dc8fbd250204a8019395f90373b48e6a8b6243
Carlos Heitor <c.heitordiniz@gmail.com>
2020-06-20 01:08:34 -0300

    Adjust remove and list methods.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,84 @@
-import React, { useEffect, useState } from "react";
-
-import api from "./services/api";
+import React, { useEffect, useState, ChangeEvent, FormEvent } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    api.get("repositories").then((response) => {
-      setRepositories(response.data);
-    });
+    api
+      .get("repositories")
+      .then((response) => {
+        setRepositories(response.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }, []);
 
-  useEffect(() => {
-    api.get("repositories").then((response) => {
-      setRepositories(response.data);
-    });
-  }, [handleRemoveRepository]);
+  async function handleAddRepository(event: FormEvent) {
+    event.preventDefault();
 
-  async function handleAddRepository() {
-    const repository = {
-      url: "https://github.com/Rocketseat/umbriel",
-      title: `Umbriel ${Date.now()}`,
-      techs: ["Node", "Express", "TypeScript"],
-      likes: 0,
-    };
-    api.post("repositories", repository).then((response) => {
-      console.log(response);
-    });
+    const title = document.getElementById("title").value;
+    const url = document.getElementById("url").value;
+    const techsString = document.getElementById("techs").value;
+    const techs = techsString.split(",");
+
+    api
+      .post("repositories", { title, url, techs })
+      .then((response) => {
+        setRepositories([...repositories, response.data]);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }
 
   async function handleRemoveRepository(id) {
-    api.delete(`/repositories/${id}`).then((response) => {
-      setRepositories(response.data);
-    });
+    try {
+      await api.delete(`repositories/${id}`);
+      setRepositories(
+        repositories.filter((repository) => repository.id !== id)
+      );
+    } catch (err) {
+      console.log(`Cannot list repositories. Cause ${err.message}.`);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        {repositories &&
-          repositories.map((repo) => {
-            return (
-              <li key={repo.id}>
-                {repo.title}
-                <button onClick={() => handleRemoveRepository(repo.id)}>
-                  Remover
-                </button>
-              </li>
-            );
-          })}
+        {repositories.length > 0 &&
+          repositories.map((repository) => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form onSubmit={handleAddRepository}>
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+          <div className="field">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" />
+          </div>
+          <div>
+            <label htmlFor="url">url</label>
+            <input type="text" name="url" id="url" />
+          </div>
+          <div>
+            <label htmlFor="techs">Techs</label>
+            <input type="text" name="techs" id="techs" />
+          </div>
+        </fieldset>
+        <button>Adicionar</button>
+      </form>
     </div>
   );
 }
